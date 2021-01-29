e6ba1b64c164c70324c2498ac94943827f7dd883
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-29 14:01:21 +0100

    update

--- lib/router.rb
+++ lib/router.rb
@@ -13,6 +13,7 @@ class Router
       puts "What do you want to do?"
       puts "1 = Write a gossip"
       puts "2 = Show all gossips"
+      puts "3 = Delete gossip(s)"
       puts "4 = Quit the app"
       params = gets.chomp.to_i
 
@@ -24,6 +25,22 @@ class Router
       when 2
         puts "You decided to show all gossips"
         @controller.index_gossips
+
+      when 3
+        puts "Please select gossips to delete:"
+        @controller.index_gossips
+        puts "You have selected the following gossips to delete:"
+        @controller.index_gossips_to_delete
+        puts "Would you like to proceed?"
+        puts "Enter Y for Yes of N for NO"
+        print "> "
+        choice = gets.chomp
+        if choice == "Y"
+          @controller.delete_gossips
+          puts index_gossips - delete_gossips
+        else
+          puts "Wise choice"
+          @controller.create_gossip
       when  4
         puts "See you later ^^"
         break
